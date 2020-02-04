1d4bf5932f4f953da7af9d3a941f1377baff7d48
OriolBonjoch <oriol.bonjoch@gmail.com>
2020-02-04 16:19:09 +0100

    Helm updated for multiplayer features

--- Source/Services/RPSLS.Web/Controllers/TokenController.cs
+++ Source/Services/RPSLS.Web/Controllers/TokenController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RPSLS.Web.Clients;
-using System.Collections.Generic;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace RPSLS.Web.Controllers
@@ -24,21 +21,21 @@ namespace RPSLS.Web.Controllers
         }
 
         [HttpGet("{token}")]
-        //public IActionResult JoinGameAsync(string token)
-        public async Task<IActionResult> JoinGameAsync(string token)
+        public IActionResult JoinGameAsync(string token)
+        //public async Task<IActionResult> JoinGameAsync(string token)
         {
             var redirect = $"{VALIDATE_URL}/{token}";
 
             //// TODO: remove code
-            var claims = new List<Claim> {
-                new Claim(ClaimTypes.Name, "Paco")
-            };
+            //var claims = new List<Claim> {
+            //    new Claim(ClaimTypes.Name, "Paco")
+            //};
 
-            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-            var principal = new ClaimsPrincipal(claimsIdentity);
-            await HttpContext.SignInAsync(principal);
-            return Redirect(redirect);
-            //return Challenge(new AuthenticationProperties { RedirectUri = redirect }, "Twitter");
+            //var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            //var principal = new ClaimsPrincipal(claimsIdentity);
+            //await HttpContext.SignInAsync(principal);
+            //return Redirect(redirect);
+            return Challenge(new AuthenticationProperties { RedirectUri = redirect }, "Twitter");
         }
 
         [HttpGet("validate/{token}")]
